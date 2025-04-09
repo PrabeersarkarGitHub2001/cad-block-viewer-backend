1fb89ca86f8791a673edbf5b19f1c6ca983cdcb0
Prabeer Sarkar <prabeersarkar2001@gmail.com>
2025-04-10 01:38:16 +0530

    fdfd

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/blocks', blockRoutes);
 
 const PORT = process.env.PORT || 5000;
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
